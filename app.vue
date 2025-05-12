cf6fa06a2f15be00876b2eea87a19ba2658c2c9d
jay <jay@ocard.co>
2025-05-12 15:47:12 +0800

    WIP

--- app.vue
+++ app.vue
@@ -1,14 +1,17 @@
 <template>
   <v-app>
-    <CommonHeader />
+    <CommonHeader v-if="showHeaderFooter" />
 
     <v-main>
       <NuxtLayout />
     </v-main>
 
-    <CommonFooter />
+    <CommonFooter v-if="showHeaderFooter" />
   </v-app>
 </template>
 
 <script setup>
+import { useRoute } from 'vue-router';
+const route = useRoute();
+const showHeaderFooter = computed(() => route.path !== '/swagger');
 </script>
