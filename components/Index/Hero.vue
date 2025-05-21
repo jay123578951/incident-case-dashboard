34770f7182724d432acfae88df084b4e239ccd02
jay <jay@ocard.co>
2025-05-21 17:33:25 +0800

    WIP

--- components/Index/Hero.vue
+++ components/Index/Hero.vue
@@ -1,13 +1,13 @@
 <template>
   <div v-bind="$attrs">
     <section class="flex flex-col items-center">
-      <CommonLogo class="mb-4 hidden lg:flex" />
+      <!-- <CommonLogo class="mb-4 hidden lg:flex" /> -->
 
       <div
         class="rounded-lg border-b-4 border-e-4 border-[#009299] bg-gradient-to-r from-[#64D1BD] to-[#009EA6] px-5 py-4 lg:px-4 lg:py-5"
       >
         <h1 class="text-[28px] font-bold text-white md:text-[32px] lg:text-5xl">
-          山域事故案件儀錶板
+          {{ title }}
         </h1>
       </div>
     </section>
@@ -18,4 +18,7 @@
 defineOptions({
   inheritAttrs: false
 });
+
+const title = ref('儀錶板');
+// const title = ref('山域事故案件儀錶板');
 </script>
--- components/common/Footer.vue
+++ components/common/Footer.vue
@@ -1,5 +1,11 @@
 <template>
   <v-footer color="#7C8494" class="justify-center py-3 text-sm">
-    Copyright © 2025 內政部消防署. All rights reserved.
+    Copyright © {{ year }} {{ name }}. All rights reserved.
   </v-footer>
 </template>
+
+<script setup>
+const year = ref(new Date().getFullYear());
+const name = ref('');
+// const name = ref('內政部消防署');
+</script>
